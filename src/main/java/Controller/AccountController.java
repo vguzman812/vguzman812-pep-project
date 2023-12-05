9cc740ee58dea461771456148b758257243ff25e
Vincent Guzman <vguzman812@gmail.com>
2023-12-05 02:34:15 +0000

    implement controllers

--- src/main/java/Controller/AccountController.java
+++ src/main/java/Controller/AccountController.java
@@ -1,27 +1,86 @@
 package Controller;
 
 import Service.AccountService;
-import io.javalin.Javalin;
-import io.javalin.http.Context;
 import Model.Account;
 
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.Optional;
+
+/**
+ * Controller class responsible for handling HTTP requests related to Account
+ * operations.
+ * Utilizes AccountService to perform business logic.
+ */
 public class AccountController {
 
     private final AccountService accountService = new AccountService();
 
+    /**
+     * Attaches route handlers for account-related endpoints to the Javalin app.
+     *
+     * @param app The Javalin application instance to which the routes are attached.
+     */
     public void attachRoutes(Javalin app) {
         app.post("/register", this::handleRegister);
         app.post("/login", this::handleLogin);
     }
 
+    /**
+     * Handles account registration requests.
+     * Parses the request body to an Account object,
+     * validates the input, and registers a new account if
+     * valid input and no other user exists.
+     * Responds with the created account or an error message.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleRegister(Context ctx) {
-        // Parse the request body to create an Account
-        // Call accountService.create() and handle responses (success/error)
+        try {
+            Account account = ctx.bodyAsClass(Account.class);
+
+            if (account.getUsername() == null || account.getUsername().isEmpty() ||
+                    account.getPassword() == null || account.getPassword().length() < 4) {
+                ctx.status(400).result("Invalid username or password");
+                return;
+            }
+
+            if (accountService.findByUsername(account.getUsername()).isPresent()) {
+                ctx.status(400).result("Username already exists");
+                return;
+            }
+
+            Account createdAccount = accountService.create(account);
+            ctx.status(200).json(createdAccount);
+        } catch (Exception e) {
+            ctx.status(500).result("An error occurred while registering.");
+        }
     }
 
+    /**
+     * Handles login requests.
+     * Validates the provided credentials against stored accounts.
+     * If successful, responds with the account details;
+     * otherwise, responds with an error.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleLogin(Context ctx) {
-        // Validate login credentials
-        // Call accountService.findByUsername() and compare passwords
-        // Respond with account details or error
+        try {
+            Account loginAccount = ctx.bodyAsClass(Account.class);
+            Optional<Account> accountOptional = accountService.findByUsername(loginAccount.getUsername());
+
+            if (accountOptional.isPresent()
+                    && BCrypt.checkpw(loginAccount.getPassword(), accountOptional.get().getPassword())) {
+                ctx.status(200).json(accountOptional.get());
+            } else {
+                ctx.status(401).result("Invalid login credentials");
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("An error occurred while logging in.");
+        }
     }
 }
--- src/main/java/Controller/MessageController.java
+++ src/main/java/Controller/MessageController.java
@@ -1,14 +1,27 @@
 package Controller;
 
 import Service.MessageService;
-import io.javalin.Javalin;
-import io.javalin.http.Context;
 import Model.Message;
 
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This class handles the web endpoints related to message operations.
+ * It utilizes the MessageService for business logic and data access.
+ */
 public class MessageController {
 
     private final MessageService messageService = new MessageService();
 
+    /**
+     * Attaches the routes to the provided Javalin app.
+     *
+     * @param app The Javalin application to attach routes to.
+     */
     public void attachRoutes(Javalin app) {
         app.post("/messages", this::handleCreateMessage);
         app.get("/messages", this::handleGetAllMessages);
@@ -18,27 +31,131 @@ public class MessageController {
         app.get("/accounts/:account_id/messages", this::handleGetMessagesByUserId);
     }
 
+    /**
+     * Handles creating a new message.
+     * Parses request body to create a Message object,
+     * validates it, and if valid, creates a new message in the database.
+     * Responds with the newly created message or an error message.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleCreateMessage(Context ctx) {
-        // Parse request for new message, validate, and create
+        try {
+            Message message = ctx.bodyAsClass(Message.class);
+            if (message.getMessage_text() == null || message.getMessage_text().length() >= 255) {
+                ctx.status(400).result("Invalid message content");
+                return;
+            }
+            // Assuming posted_by is validated elsewhere (e.g., via authentication)
+            Message createdMessage = messageService.create(message);
+            ctx.status(201).json(createdMessage);
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while processing message creation");
+        }
     }
 
+    /**
+     * Handles requests for retrieving all messages.
+     * Responds with a list of all messages from the database or an error message
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleGetAllMessages(Context ctx) {
-        // Retrieve and respond with all messages
+        try {
+            List<Message> messages = messageService.getAll();
+            ctx.json(messages);
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while fetching all messages");
+        }
     }
 
+    /**
+     * Handles retrieving a specific message by its ID.
+     * Responds with the requested message if found,
+     * or a not found/error message if the message does not exist or an issue
+     * occurs.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleGetMessageById(Context ctx) {
-        // Extract message ID from path and retrieve specific message
+        try {
+            int messageId = Integer.parseInt(ctx.pathParam("message_id"));
+            Optional<Message> message = messageService.get(messageId);
+            if (message.isPresent()) {
+                ctx.json(message.get());
+            } else {
+                ctx.status(404).result("Message not found");
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while fetching message by ID");
+        }
     }
 
+    /**
+     * Handles deleting a message by its ID.
+     * Responds with the deleted message if successful,
+     * no content if the message does not exist,
+     * or an error message if an issue occurs.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleDeleteMessage(Context ctx) {
-        // Extract message ID and delete the message
+        try {
+            int messageId = Integer.parseInt(ctx.pathParam("message_id"));
+            Optional<Message> deletedMessage = messageService.delete(messageId);
+            if (deletedMessage.isPresent()) {
+                ctx.json(deletedMessage.get());
+            } else {
+                ctx.status(204); // No Content
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while deleting message");
+        }
     }
 
+    /**
+     * Handles updating a specific message's details by its ID.
+     * Responds with the updated message if successful,
+     * a not found message if the message does not exist,
+     * or an error message if an issue occurs.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleUpdateMessage(Context ctx) {
-        // Extract message ID and update details
+        try {
+            int messageId = Integer.parseInt(ctx.pathParam("message_id"));
+            Message updatedInfo = ctx.bodyAsClass(Message.class);
+            if (updatedInfo.getMessage_text() == null || updatedInfo.getMessage_text().length() >= 255) {
+                ctx.status(400).result("Invalid message content");
+                return;
+            }
+            Optional<Message> existingMessage = messageService.get(messageId);
+            if (existingMessage.isPresent()) {
+                Message messageToUpdate = existingMessage.get();
+                messageToUpdate.setMessage_text(updatedInfo.getMessage_text());
+                Message updatedMessage = messageService.update(messageToUpdate);
+                ctx.json(updatedMessage);
+            } else {
+                ctx.status(404).result("Message not found");
+            }
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while updating message");
+        }
     }
 
+    /**
+     * Handles requests for retrieving all messages posted by a specific user.
+     * Responds with a list of messages or an error message if an issue occurs.
+     *
+     * @param ctx The context object representing the HTTP request and response.
+     */
     private void handleGetMessagesByUserId(Context ctx) {
-        // Extract user ID from path and retrieve messages by that user
+        try {
+            int userId = Integer.parseInt(ctx.pathParam("account_id"));
+            List<Message> messages = messageService.getAllByUserId(userId);
+            ctx.json(messages);
+        } catch (Exception e) {
+            ctx.status(500).result("Server error while fetching messages by user ID");
+        }
     }
 }
